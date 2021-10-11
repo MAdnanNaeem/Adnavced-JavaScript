4cbc63863eee91c6511ec6b93f46044fda29a493
MAdnanNaeem <madnannaeem@hotmail.com>
2021-10-12 00:08:00 +0500

    stage_1

---
+++ The Complete JavaScript Course 2021 From Zero to Expert/1. Js Fundamentals Part 1/Practice Assignments/1. Values and Variables .js
@@ -0,0 +1,6 @@
+let country = "Pakistan", continent = "Asia", population = "22 Billion";
+
+console.log(" Country: " + country + "\n"+
+  " Continent: " + continent + "\n"+
+  " Population: " + population
+);
