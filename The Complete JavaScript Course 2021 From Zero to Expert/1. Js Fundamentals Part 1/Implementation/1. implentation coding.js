1580aa2707bef70ac1b4f9218eb75db341feba12
MAdnanNaeem <madnannaeem@hotmail.com>
2021-12-09 01:14:57 +0500

    commit 1

--- The Complete JavaScript Course 2021 From Zero to Expert/1. Js Fundamentals Part 1/Implementation/1. implentation coding.js
+++ The Complete JavaScript Course 2021 From Zero to Expert/1. Js Fundamentals Part 1/Implementation/1. implentation coding.js
@@ -7,3 +7,29 @@ let check = true;
 console.log(typeof uName);
 console.log(typeof x);
 console.log(typeof check);
+
+// TODO: Mathematical Operators
+
+const thisYear = 2021;
+const adnanAge = thisYear - 1998;
+const farooqAge = thisYear - 1999;
+console.log(adnanAge, farooqAge);
+
+// * Exponent-operator
+const squareValue = 2 ** 3; //2^3
+console.log(squareValue);
+
+// * String Concatination
+const demoString1 = `Allah`;
+const demoString2 = `Hu`;
+console.log(demoString1 + ` ` + demoString2);
+console.log(`${demoString1} ${demoString2}`);
+
+// * Assignment Operator
+let num = 10 + 5;
+num++;
+++num;
+num += 1;
+num *= 1; //18
+num /= 2; // 18/2
+console.log(num); // 9
